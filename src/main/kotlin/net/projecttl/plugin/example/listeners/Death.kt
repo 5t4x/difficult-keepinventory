1b27a9595f0a92526f0cdcbd9ce3efd67724ca98
5t4x <fivetfourx@outlook.com>
2023-06-03 10:52:31 -0400

    damage items in main and off hands

--- src/main/kotlin/net/projecttl/plugin/example/listeners/Death.kt
+++ src/main/kotlin/net/projecttl/plugin/example/listeners/Death.kt
@@ -1,40 +1,60 @@
 package net.projecttl.plugin.example.listeners
 
 import net.projecttl.plugin.example.INGREDIENTS
+import org.bukkit.entity.LivingEntity
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.PlayerDeathEvent
+import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.meta.Damageable
+import kotlin.math.floor
 import kotlin.math.round
 
 class Death : Listener {
+    /**
+     * Remove 1/5 of the item's durability
+     * @return The new ItemStack
+     */
+    private fun damageMain(item: ItemStack, livingEntity: LivingEntity): ItemStack? {
+        if (item.itemMeta == null) return null
+
+        val damageable = item.itemMeta as Damageable
+        val remainingDurability = item.type.maxDurability - damageable.damage
+        // always floor it so damage can be 0
+        val takeDamage = floor((remainingDurability / 5).toDouble()).toInt()
+
+        return item.damage(takeDamage, livingEntity)
+    }
+
     @EventHandler
     fun onDeath(event: PlayerDeathEvent) {
         // only execute logic if they're keeping their inventory
-        if (event.keepInventory) for (item in event.player.inventory) {
-            if (item == null) continue
+        if (event.keepInventory) {
+            event.player.inventory.setItemInMainHand(damageMain(event.player.inventory.itemInMainHand, event.player))
+            event.player.inventory.setItemInOffHand(damageMain(event.player.inventory.itemInOffHand, event.player))
 
-            // if there's only one, we shouldn't have to drop it
-            if (item.amount == 1) continue
+            for (item in event.player.inventory) {
+                if (item == null) continue
 
-            // only drop ingredients
-            if (!INGREDIENTS.contains(item.type.name)) continue
+                // if there's only one, we shouldn't have to drop it
+                if (item.amount == 1) continue
 
-            // drop half the stack
-            val newAmount = round((item.amount / 2).toDouble()).toInt()
-            val dropAmount = item.amount - newAmount
+                // only drop ingredients
+                if (INGREDIENTS.contains(item.type.name)) {
+                    // drop half the stack
+                    val newAmount = round((item.amount / 2).toDouble()).toInt()
+                    val dropAmount = item.amount - newAmount
 
-            // drop what belongs to the player
-            val dropStack = item.clone()
-            dropStack.amount = dropAmount
-            /*event.player.world.dropItem(
-                event.player.location,
-                dropStack
-            )*/
+                    // drop what belongs to the player
+                    val dropStack = item.clone()
+                    dropStack.amount = dropAmount
 
-            event.drops += dropStack
+                    event.drops += dropStack
 
-            // set the amount in the player's inventory
-            item.amount = newAmount
+                    // set the amount in the player's inventory
+                    item.amount = newAmount
+                }
+            }
         }
     }
 }
